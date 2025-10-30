6e00da27e1fcf55f5087b67363a755cb8166891c
Sensol2 <hhncn4471@soongsil.ac.kr>
2025-10-30 12:50:51 +0900

    feat: font 추가

--- mipick/app/main/styles/LocalStyle.ts
+++ mipick/app/main/styles/LocalStyle.ts
@@ -3,17 +3,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const LocalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
+
   body {
     margin: 0;
     padding: 0;
-    font-family: Arial, sans-serif;
+    font-family: 'DM Sans', Arial, sans-serif;
   }
 
   h1, h2, h3 {
     margin: 0;
+    font-family: 'DM Sans', Arial, sans-serif;
   }
 
   p {
     margin: 0;
+    font-family: 'DM Sans', Arial, sans-serif;
   }
 `;
